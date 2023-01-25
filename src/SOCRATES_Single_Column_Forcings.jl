e90451b282d5aa90640a42b2cffde70d5b0aee36
Jordan Benjamin <jbenjami@sampo.gps.caltech.edu>
2023-01-24 20:57:22 -0800

    remove the local julia depot commands cause they dont work wellwith distributing a package

--- src/SOCRATES_Single_Column_Forcings.jl
+++ src/SOCRATES_Single_Column_Forcings.jl
@@ -1,8 +1,8 @@
 module SOCRATES_Single_Column_Forcings
 
-# put these first so are avail everywhere
-THIS_DIR="/home/jbenjami/Research_Schneider/CliMa/SOCRATES_Single_Column_Forcings.jl";
-empty!(DEPOT_PATH); push!(DEPOT_PATH,THIS_DIR*"/.julia_depot"); 
+# put these first so are avail everywhere (removed cause doesn't work if you're say loading the package from github)
+# THIS_DIR="/home/jbenjami/Research_Schneider/CliMa/SOCRATES_Single_Column_Forcings.jl";
+# empty!(DEPOT_PATH); push!(DEPOT_PATH,THIS_DIR*"/.julia_depot"); 
 using Pkg
 # Pkg.develop(path="/home/jbenjami/Research_Schneider/CliMa/Thermodynamics.jl") # do i still need this?
 Pkg.add(url="https://github.com/CliMA/Thermodynamics.jl#jb/non_eq_moisture")
@@ -26,7 +26,5 @@ include("../Data/Atlas_LES_Profiles/download_atlas_les_profiles.jl")
 include("../Data/Atlas_LES_Profiles/open_atlas_les_profiles.jl")
 include("helper_functions.jl")
 include("process_case.jl")
-
-
 
 end
