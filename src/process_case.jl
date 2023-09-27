8e55f9e89c3864a350a8eac76172984d46d8a129
Jordan Benjamin <jbenjami@sampo.gps.caltech.edu>
2023-09-26 18:50:25 -0700

    i think for obs we really do just leave the obs file

--- src/process_case.jl
+++ src/process_case.jl
@@ -95,53 +95,53 @@ function process_case(
     tsg     = map((pg,Tg,qg)->TD.PhaseEquil_pTq.(thermo_params, pg , Tg , qg ), pg,Tg,qg)
 
     # get indices where the ground would get inserted in... (and convert to same shape/dims.)
-    ground_indices = map( (ts) -> get_ground_insertion_indices(ts, tsg[:ERA5_data], z_dim_num; thermo_params, data=data), ts) # always use ERA5 surface...
+    ground_indices = map( (ts) -> get_ground_insertion_indices(ts, tsg[forcing], z_dim_num; thermo_params, data=data), ts) # always use ERA5 surface...
     # @show(ground_indices, map(size,ground_indices))
 
     q_full   = combine_air_and_ground_data(q[forcing], qg[forcing], z_dim_num; insert_location=ground_indices[forcing]) # full q_array/qt_nudge -- is used from the chosen forcing dataset
     qt_nudge = q_full
 
 
-    ts_full = map((ts,ground_indices)->combine_air_and_ground_data(ts, tsg[:ERA5_data], z_dim_num; insert_location=ground_indices), ts, ground_indices)
+    ts_full = map((ts,ground_indices)->combine_air_and_ground_data(ts, tsg[forcing], z_dim_num; insert_location=ground_indices), ts, ground_indices) # is it era5? their output les plots sure don't look it...
 
     # old_z  => Precompute old z coordinate (precompute to save us some trouble later (get_data_new_z_t func can self-calculate it but it's redundant to keep calculating z)
     # z_old = map((ts,tsg,data)->lev_to_z( ts,tsg; thermo_params, data=data) , ts,tsg, data) # should this be tsg[:ERA5_data] cause surface is always ERA5
-    z_old = map((ts,data)->lev_to_z( ts,tsg[:ERA5_data]; thermo_params, data=data) , ts, data) # should this be tsg[:ERA5_data] cause surface is always ERA5
+    z_old = map((ts,data)->lev_to_z( ts,tsg[forcing]; thermo_params, data=data) , ts, data) # should this be tsg[:ERA5_data] cause surface is always ERA5 (is it?)
     # @show(z_old)
 
 
     # ω (subsidence) # always forced by era5
-    ρ  = TD.air_density.(thermo_params, ts[:ERA5_data])
-    ρg = TD.air_density.(thermo_params,tsg[:ERA5_data])
-    ρ  = combine_air_and_ground_data(ρ, ρg, z_dim_num; insert_location=ground_indices[:ERA5_data])
-    ω  = data[:ERA5_data]["omega"]
-    dpdt_g = data[:ERA5_data]["Ptend"]
+    ρ  = TD.air_density.(thermo_params, ts[forcing])
+    ρg = TD.air_density.(thermo_params,tsg[forcing])
+    ρ  = combine_air_and_ground_data(ρ, ρg, z_dim_num; insert_location=ground_indices[forcing])
+    ω  = data[forcing]["omega"]
+    dpdt_g = data[forcing]["Ptend"]
     dpdt_g = add_dim(dpdt_g, z_dim_num) # should be lon lat lev time (hopefully order was already correct)
-    ω  = combine_air_and_ground_data(ω, dpdt_g, z_dim_num; insert_location=ground_indices[:ERA5_data])
+    ω  = combine_air_and_ground_data(ω, dpdt_g, z_dim_num; insert_location=ground_indices[forcing])
 
-    p_grid = add_dim(align_along_dimension(p[forcing], z_dim_num),time_dim_num) # align on dimension 3 lev, add time dimensino 4
-    p_full = combine_air_and_ground_data(p_grid, pg[forcing][:] ,z_dim_num; insert_location=ground_indices[:ERA5_data]) # align p along lev, add lev_dim to pg, stack (hope braodcasting works to fill it out...)
+    p_grid = map((p,)->add_dim(align_along_dimension(p, z_dim_num),time_dim_num), p) # align on dimension 3 lev, add time dimensino 4
+    p_full = map((p_grid, pg, ground_indices)->combine_air_and_ground_data(p_grid, pg[:] ,z_dim_num; insert_location=ground_indices), p_grid, pg, ground_indices) # align p along lev, add lev_dim to pg, stack (hope braodcasting works to fill it out...)
 
     # this i think was wrong -- maybe get atlas to confirm what version of the sigmoid she used.
     # c    = 100
     # a    = -2 + exp(250/c)
     # f_p  = @. 2(a+1) / (a+exp(p_full/c)) - 1
 
-    squeeze = (x) -> dropdims(x, dims = (findall(size(x) .== 1)...,))
+    # squeeze = (x) -> dropdims(x, dims = (findall(size(x) .== 1)...,))
 
     # this was derived for a scaled sigmoid passing through (ps,1), (25000 Pa, 0)
     L = 2.2 # maximum value (shape parameter)
     a = -L/2
     p0 = 250. * 100
     # @show( pg[:ERA5_data][:], size( pg[:ERA5_data][:]) )
-    p1 = add_dim(  pg[:ERA5_data][:], z_dim_num)
+    p1 = add_dim(  pg[forcing][:], z_dim_num)
     # @show(p1, size(p1))
     (p1,y1) = (p1 , 1) # use our ground pressure (can't use pfull cause would need to pull)
     (p2,y2) = (p0                , 0)
     k = @. log((L/2 + 1)/(L/2 - 1)) / (p1-p0) # ps is an array so we have an array of ks
     # @show(squeeze(k),size(k))
     # @show(size(p_full))
-    f_p = @. a + L  / (1 + exp(-k*(p_full-p0)))
+    f_p = @. a + L  / (1 + exp(-k*(p_full[forcing]-p0)))
 
 
     grav = TDP.grav(thermo_params)
@@ -158,10 +158,10 @@ function process_case(
     # @show(squeeze(subsidence)',squeeze(p_full)')
 
     # u, v # always forced by ERA5
-    u = data[:ERA5_data]["u"]
-    v = data[:ERA5_data]["v"]
-    u = combine_air_and_ground_data(u,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
-    v = combine_air_and_ground_data(v,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
+    u = data[forcing]["u"]
+    v = data[forcing]["v"]
+    u = combine_air_and_ground_data(u,FT(0),z_dim_num; insert_location=ground_indices[forcing])
+    v = combine_air_and_ground_data(v,FT(0),z_dim_num; insert_location=ground_indices[forcing])
     u_nudge,v_nudge = u,v
 
     #=
@@ -173,10 +173,10 @@ function process_case(
     =#
 
     # u_g, v_g # always forced by ERA5
-    ug = data[:ERA5_data]["ug"]
-    vg = data[:ERA5_data]["vg"]
-    ug = combine_air_and_ground_data(ug,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
-    vg = combine_air_and_ground_data(vg,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
+    ug = data[forcing]["ug"]
+    vg = data[forcing]["vg"]
+    ug = combine_air_and_ground_data(ug,FT(0),z_dim_num; insert_location=ground_indices[forcing])
+    vg = combine_air_and_ground_data(vg,FT(0),z_dim_num; insert_location=ground_indices[forcing])
     ug_nudge,vg_nudge = ug,vg
 
     # H (nudge)
@@ -185,25 +185,25 @@ function process_case(
 
 
     # dTdt_hadv (i think these are supposed to be always ERA5)
-    dTdt_hadv = data[:ERA5_data]["divT"]
-    dTdt_hadv = combine_air_and_ground_data(dTdt_hadv,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
+    dTdt_hadv = data[forcing]["divT"]
+    dTdt_hadv = combine_air_and_ground_data(dTdt_hadv,FT(0),z_dim_num; insert_location=ground_indices[forcing])
 
     # dqdt_hadv (i think these are supposed to be always ERA5)
-    dqtdt_hadv = data[:ERA5_data]["divq"]
-    dqtdt_hadv = combine_air_and_ground_data(dqtdt_hadv,FT(0),z_dim_num; insert_location=ground_indices[:ERA5_data])
+    dqtdt_hadv = data[forcing]["divq"]
+    dqtdt_hadv = combine_air_and_ground_data(dqtdt_hadv,FT(0),z_dim_num; insert_location=ground_indices[forcing])
     # expand everything to new z grid and make t operation (initial condition or time splines)
-    # dTdt_hadv  = get_data_new_z_t(dTdt_hadv , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
-    dTdt_hadv  = get_data_new_z_t(dTdt_hadv  .* (forcing==:ERA5_data) , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)  # testing not using forcing for obs to see if fixes anything
+    # dTdt_hadv  = get_data_new_z_t(dTdt_hadv , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
+    dTdt_hadv  = get_data_new_z_t(dTdt_hadv  .* (forcing==forcing) , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)  # testing not using forcing for obs to see if fixes anything
     H_nudge    = get_data_new_z_t(H_nudge   , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing]   , data=data[forcing]   , thermo_params,  initial_condition)
-    # dqtdt_hadv = get_data_new_z_t(dqtdt_hadv, new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
-    dqtdt_hadv = get_data_new_z_t(dqtdt_hadv .* (forcing==:ERA5_data) , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition) # testing not using forcing for obs to see if fixes anything
+    # dqtdt_hadv = get_data_new_z_t(dqtdt_hadv, new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
+    dqtdt_hadv = get_data_new_z_t(dqtdt_hadv .* (forcing==forcing) , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition) # testing not using forcing for obs to see if fixes anything
     qt_nudge   = get_data_new_z_t(qt_nudge  , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing]   , data=data[forcing]   , thermo_params,  initial_condition)
-    subsidence = get_data_new_z_t(subsidence, new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
-    u_nudge    = get_data_new_z_t(u_nudge   , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
-    v_nudge    = get_data_new_z_t(v_nudge   , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
+    subsidence = get_data_new_z_t(subsidence, new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
+    u_nudge    = get_data_new_z_t(u_nudge   , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
+    v_nudge    = get_data_new_z_t(v_nudge   , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
 
-    ug_nudge   = get_data_new_z_t(ug_nudge  , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
-    vg_nudge   = get_data_new_z_t(vg_nudge  , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[:ERA5_data], data=data[:ERA5_data], thermo_params,  initial_condition)
+    ug_nudge   = get_data_new_z_t(ug_nudge  , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
+    vg_nudge   = get_data_new_z_t(vg_nudge  , new_z, z_dim_num,time_dim_num, flight_number; z_old = z_old[forcing], data=data[forcing], thermo_params,  initial_condition)
 
     return  (; dTdt_hadv, H_nudge, dqtdt_hadv, qt_nudge, subsidence, u_nudge, v_nudge, ug_nudge, vg_nudge)
 
